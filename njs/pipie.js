164bc17ac5d1e16ff9e2658cbe93c85fe0fd609b
pisenliang <pisenliang@gmail.com>
2020-08-21 14:23:02 +0800

    调整结构

---
+++ njs/pipie.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Pi Patle
+ * @Email: pisenliang@gmail.com
+ * @Github: https://github.com/SenLiangpi
+ * @Website: https://senliangpi.github.io/blog/#/
+ * @Date: 2020-08-21 11:14:50
+ * @LastEditors: Pi Patle
+ * @LastEditTime: 2020-08-21 11:16:04
+ */
+window.pipiaccountId = '6000000001016007';
