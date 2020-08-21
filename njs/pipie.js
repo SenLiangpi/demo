1009d5bd34581a395d899d1be60b62991f780f48
pisenliang <pisenliang@gmail.com>
2020-08-21 15:03:30 +0800

    测试

--- njs/pipie.js
+++ njs/pipie.js
@@ -5,6 +5,8 @@
  * @Website: https://senliangpi.github.io/blog/#/
  * @Date: 2020-08-21 11:14:50
  * @LastEditors: Pi Patle
- * @LastEditTime: 2020-08-21 11:16:04
+ * @LastEditTime: 2020-08-21 15:03:07
  */
-window.pipiaccountId = '6000000001016007';
+export const webLoggingPurview = (value)=>{
+  return ['6000000001016007'].includes(value)
+}
