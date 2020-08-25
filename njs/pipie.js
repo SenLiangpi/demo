1cdbc7d8f533701550d5c4f10a281baf8b9d1773
pisenliang <pisenliang@gmail.com>
2020-08-25 15:19:07 +0800

    增加权限

--- njs/pipie.js
+++ njs/pipie.js
@@ -5,8 +5,9 @@
  * @Website: https://senliangpi.github.io/blog/#/
  * @Date: 2020-08-21 11:14:50
  * @LastEditors: Pi Patle
- * @LastEditTime: 2020-08-21 15:08:53
+ * @LastEditTime: 2020-08-25 15:18:29
  */
 const webLoggingPurview = function(value){
-  return ['6000000001016007'].includes(value)
+  let arr = ['6000000001016007','6000000001018880']
+  return arr.includes(value)
 }
