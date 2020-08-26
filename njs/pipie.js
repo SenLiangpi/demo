c39e9e22184564e56a63f9ededa6c101f58782cf
pisenliang <pisenliang@gmail.com>
2020-08-26 09:17:14 +0800

    master

--- njs/pipie.js
+++ njs/pipie.js
@@ -5,9 +5,9 @@
  * @Website: https://senliangpi.github.io/blog/#/
  * @Date: 2020-08-21 11:14:50
  * @LastEditors: Pi Patle
- * @LastEditTime: 2020-08-25 15:18:29
+ * @LastEditTime: 2020-08-26 09:15:33
  */
 const webLoggingPurview = function(value){
-  let arr = ['6000000001016007','6000000001018880']
+  let arr = ['dev6000000001016007','dev6000000001018880']
   return arr.includes(value)
 }
